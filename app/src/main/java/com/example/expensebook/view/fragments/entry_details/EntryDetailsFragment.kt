e83ec8d8aba328d344e63dc330a0b72a9df5ceb9
Luan Patrick Alves Poi <luan.poi@cits.br>
2023-01-07 01:31:30 -0300

    Corrigindo viewmodel para apenas uma variavel (uiState)

--- app/src/main/java/com/example/expensebook/view/fragments/entry_details/EntryDetailsFragment.kt
+++ app/src/main/java/com/example/expensebook/view/fragments/entry_details/EntryDetailsFragment.kt
@@ -1,14 +1,17 @@
 package com.example.expensebook.view.fragments.entry_details
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.ViewModel
 import androidx.navigation.fragment.findNavController
 import com.example.expensebook.databinding.FragmentEntryDetailsBinding
 import com.example.expensebook.model.entity.Entry
+import com.example.expensebook.repository.EntryRepository
 import com.example.expensebook.view.fragments.home.HomeViewModel
 import java.lang.Float
 import java.time.OffsetDateTime
@@ -17,11 +20,13 @@ import java.time.format.DateTimeFormatter
 class EntryDetailsFragment : Fragment() {
 
     private lateinit var binding: FragmentEntryDetailsBinding
-    private lateinit var entryDetailViewModel: EntryDetailsViewModel
+
+    private val entryDetailViewModel: EntryDetailsViewModel by activityViewModels {
+        EntryDetailsViewModel.Factory(EntryRepository(requireActivity().application))
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        entryDetailViewModel = EntryDetailsViewModel(requireActivity().application)
     }
 
     override fun onCreateView(
@@ -35,28 +40,32 @@ class EntryDetailsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        entryDetailViewModel.entry.value.let {
-            if (it != null){
-                binding.textViewDate.setText(it.date.format(DateTimeFormatter.ofPattern("dd/MM/yyyy")))
-                binding.switchEntryType.isChecked = (it.value >= 0f) //true=receipt | false=expense
-                binding.editTextValue.setText(it.value.toString())
-                binding.textInputDescription.editText?.setText(it.description)
-            }
+        entryDetailViewModel.stateOnceAndStream().observe(viewLifecycleOwner){ uiState ->
+            bindUiState(uiState)
+        }.also {
+            binding.switchEntryType.setOnClickListener { entryDetailViewModel.toggleEntryType(binding.switchEntryType.isChecked)}
         }
 
-        binding.buttonConfirm.setOnClickListener { save().also { findNavController().navigateUp() } }
+        binding.buttonConfirm.setOnClickListener { onSave().also { findNavController().navigateUp() } }
         binding.buttonCancel.setOnClickListener {
             findNavController().navigateUp()
         }
     }
 
-    fun save(){
-        val entry = entryDetailViewModel.entry.value ?: return
-//        TODO: add date edit possibility
-        entry.value = if(binding.switchEntryType.isChecked) binding.editTextValue.text.toString().toFloat() else binding.editTextValue.text.toString().toFloat().times(-1)
-        entry.description = binding.textInputDescription.editText?.text.toString()
-        entryDetailViewModel.entry.postValue(entry)
+    fun bindUiState(uiState: EntryDetailsUiState){
+        binding.textViewDate.setText(uiState.date.format(DateTimeFormatter.ofPattern("dd/MM/yyyy")))
+        binding.switchEntryType.isChecked = (uiState.value >= 0f) //true=receipt | false=expense
+        binding.editTextValue.setText(uiState.value?.toString())
+        binding.textInputDescription.editText?.setText(uiState.description)
+    }
+
+    fun onSave(){
+        val entry: Entry = Entry(
+            null,
+            OffsetDateTime.now(),
+            if(binding.switchEntryType.isChecked) binding.editTextValue.text.toString().toFloat() else binding.editTextValue.text.toString().toFloat().times(-1),
+            binding.textInputDescription.editText?.text.toString()
+        )
         entryDetailViewModel.save(entry)
     }
 }
--- app/src/main/java/com/example/expensebook/view/fragments/entry_details/EntryDetailsViewModel.kt
+++ app/src/main/java/com/example/expensebook/view/fragments/entry_details/EntryDetailsViewModel.kt
@@ -1,23 +1,38 @@
 package com.example.expensebook.view.fragments.entry_details
 
 import android.app.Application
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.*
 import com.example.expensebook.model.entity.Entry
 import com.example.expensebook.repository.EntryRepository
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import java.time.OffsetDateTime
 
-class EntryDetailsViewModel(application: Application) : ViewModel() {
+class EntryDetailsViewModel(private val entryRepository: EntryRepository) : ViewModel() {
 
-    private val entryRepository: EntryRepository = EntryRepository(application)
+    private val uiState: MutableLiveData<EntryDetailsUiState> by lazy {
+        MutableLiveData<EntryDetailsUiState>(EntryDetailsUiState(null, OffsetDateTime.now(), false, 0f, ""))
+    }
 
-    val entry: MutableLiveData<Entry> = MutableLiveData(Entry())
+    fun stateOnceAndStream(): LiveData<EntryDetailsUiState> = uiState
+
+    fun toggleEntryType(value: Boolean){
+        uiState.value.let { currentUiState ->
+            currentUiState?.isReceipt = value
+//            uiState.value = currentUiState
+//            uiState.postValue(uiState.value)
+        }
+    }
 
     fun save(entry: Entry){
         viewModelScope.launch(Dispatchers.IO) {
             entryRepository.addEntry(entry)
+        }
+    }
+
+    class Factory(private val entryRepository: EntryRepository) : ViewModelProvider.Factory {
+        override fun <T : ViewModel> create(modelClass: Class<T>): T {
+            return EntryDetailsViewModel(entryRepository) as T
         }
     }
 }
