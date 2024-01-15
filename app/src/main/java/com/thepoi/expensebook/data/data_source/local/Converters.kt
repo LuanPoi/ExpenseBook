d2989f75f305c087b40c352163b94bfe481f002f
Luan Poi <luan_poi@hotmail.com>
2024-01-15 00:07:55 -0300

    Altera valor das entradas no banco de REAL para TEXT

--- app/src/main/java/com/thepoi/expensebook/data/data_source/local/Converters.kt
+++ app/src/main/java/com/thepoi/expensebook/data/data_source/local/Converters.kt
@@ -3,13 +3,22 @@ package com.thepoi.expensebook.data.data_source.local
 import androidx.room.TypeConverter
 import java.time.Instant
 import java.time.LocalDate
-import java.time.OffsetDateTime
 import java.time.YearMonth
 import java.time.ZoneId
-import java.time.ZoneOffset
 import java.time.ZonedDateTime
 
 class Converters {
+
+    @TypeConverter
+    fun toFloat(string: String): Float {
+        return string.toFloat()
+    }
+
+    @TypeConverter
+    fun fromFloat(float: Float): String {
+        return "%.2f".format(float)
+    }
+
     @TypeConverter
     fun toYearMonth(epochDay: Long?): YearMonth? {
         return epochDay?.let { YearMonth.from(LocalDate.ofEpochDay(epochDay)) }
--- app/src/main/java/com/thepoi/expensebook/data/data_source/local/entities/Entry.kt
+++ app/src/main/java/com/thepoi/expensebook/data/data_source/local/entities/Entry.kt
@@ -1,8 +1,8 @@
 package com.thepoi.expensebook.data.data_source.local.entities
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import java.time.OffsetDateTime
 import java.time.ZoneId
 import java.time.ZonedDateTime
 
@@ -10,6 +10,6 @@ import java.time.ZonedDateTime
 data class Entry(
     @PrimaryKey(autoGenerate = true) var id: Long? = null,
     var datetime: ZonedDateTime = ZonedDateTime.now(ZoneId.systemDefault()),
-    var amount: Float = 0f,
+    @ColumnInfo(typeAffinity = ColumnInfo.TEXT) var amount: Float = 0f,
     var description: String = ""
 )
